8b01d70fe32f47d02ece2330b92edde7296cbf37
sherwin <sherwinnss@gmail.com>
2024-01-01 12:42:24 +0800

    add representation for a single neuron

---
+++ single_neuron.py
@@ -0,0 +1,8 @@
+## representing one neuron as an output 
+inputs = [1,2,3]
+weights = [.5, .4, .3]
+bias = 1
+
+neuron_output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
+print(neuron_output) # output = 3.2 
+
